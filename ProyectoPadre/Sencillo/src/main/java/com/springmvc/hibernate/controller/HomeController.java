0a1935f1f454a3aebfc7bdf45fe99fb7e5e58143
PJ Cabezas Vallejo <pedroj.cabezasvallejo@gmail.com>
2016-02-27 20:15:43 +0100

    Subida inicial
    Proyecto basico. SpringMVC, hibernate, maven multimodulo

--- ProyectoPadre/Sencillo/src/main/java/com/springmvc/hibernate/controller/HomeController.java
+++ ProyectoPadre/Sencillo/src/main/java/com/springmvc/hibernate/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.springmvc.hibernate.controller;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvc.hibernate.core.entity.Student;
@@ -30,8 +34,30 @@ public class HomeController {
 		return new ModelAndView("home");
  	}
 	
-	@RequestMapping(value="/new")
+	@RequestMapping(value="/new", method=RequestMethod.GET )
 	public ModelAndView newStudent(ModelMap model){
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return new ModelAndView("newStudent");
+ 	}
+	
+	@RequestMapping(value="/new", method=RequestMethod.POST )
+	public String saveStudent(@Valid Student student, BindingResult result,
+            ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            return "newStudent";
+        }
+         
+        studentService.save(student);
+ 
+        model.addAttribute("success", "Student " + student.getName() + " registered successfully");
+        return "redirect:/list";
+ 	}
+	
+	
+	@RequestMapping(value="/delete")
+	public ModelAndView deleteStudent(ModelMap model){
 		Student student = new Student();
 		model.addAttribute("student", student);
 		return new ModelAndView("newStudent");
